ab05bc5ca54f44eaa7dc3020857a77a7d8503fd6
Boray Chen <r0975929562@gmail.com>
2025-02-22 16:01:09 +0800

    Initial Commit

---
+++ FriendLists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FriendLists
+//
+//  Created by Boray Chen on 2025/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FriendListsTests/FriendListsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FriendListsTests.swift
+//  FriendListsTests
+//
+//  Created by Boray Chen on 2025/2/22.
+//
+
+import Testing
+@testable import FriendLists
+
+struct FriendListsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
